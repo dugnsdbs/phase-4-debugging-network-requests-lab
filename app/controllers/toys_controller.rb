e3b80f20905a6cb8ddacfac7f05a33afcb8887be
yeohoon yoon <yhoon.yoon@gmail.com>
2022-03-16 13:53:58 -0400

    complete

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -2,18 +2,26 @@ class ToysController < ApplicationController
   wrap_parameters format: []
 
   def index
+ 
     toys = Toy.all
     render json: toys
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
+
   end
 
+ 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy
+      toy.update(likes: toy.likes + 1)
+      render json: toy
+      else
+      render json: {errors: "Toy not found"}, status: :not_found
+    end
   end
 
   def destroy
